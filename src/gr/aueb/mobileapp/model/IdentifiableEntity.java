75be911896c15c6307287289f25baf20efab3fed
Georgios Adamidis <g.adamidis1985@gmail.com>
2023-07-04 00:10:54 +0300

    v0.2

---
+++ src/gr/aueb/mobileapp/model/IdentifiableEntity.java
@@ -0,0 +1,11 @@
+package gr.aueb.mobileapp.model;
+
+@FunctionalInterface
+public interface IdentifiableEntity {
+    /**
+     * Returns the id of the entity.
+     *
+     * @return  the entity id.
+     */
+   long getId();
+}
